10dceaf7b5d802c36140dea0f8d7f26975523c7b
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-02 21:07:28 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,5 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2020": true,
-		"node": true,
-	},
-	"extends": [
-		"eslint:recommended"
+	extends: [
+		"jonathantreffler",
 	],
-	"parserOptions": {
-		"ecmaVersion": 11,
-		"sourceType": "module"
-	},
-	"plugins": [
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		]
-	}
 };
