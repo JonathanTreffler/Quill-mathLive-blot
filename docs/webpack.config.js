c46683717b3090da6c161c1fa52aab12afc92fe8
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-03 20:22:41 +0200

    Update webpack.config.js

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -6,4 +6,15 @@ module.exports = {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+     rules: [
+       {
+         test: /\.css$/,
+         use: [
+           'style-loader',
+           'css-loader',
+         ],
+       },
+     ],
+   },
 };
