31ebf4253b81720ef80f6851b3a687a7993e0943
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-03 11:04:12 +0200

    added webpack to docs

---
+++ docs/src/index.js
@@ -0,0 +1,13 @@
+import mathLiveBlot from "quill-mathlive-blot";
+
+var quill = new Quill('#editor-container', {
+    modules: {
+      toolbar: [
+        [{ header: [1, 2, false] }],
+        ['bold', 'italic', 'underline'],
+        ['image', 'code-block']
+      ]
+    },
+    placeholder: 'Compose an epic...',
+    theme: 'snow'
+});
