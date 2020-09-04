eacb3e2f41e133189a3fbe2f07f31d986f8caccb
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-04 11:09:05 +0200

    Update index.js

--- docs/src/index.js
+++ docs/src/index.js
@@ -2,32 +2,39 @@ import mathLiveBlot from "quill-mathlive-blot";
 import "./style.css";
 
 var quill = new Quill('#editor-container', {
-    modules: {
-      toolbar: [
-        [{ header: [1, 2, false] }],
-        ['bold', 'italic', 'underline'],
-        ['image', 'code-block'],
-        ['mathLive']
-      ]
-    },
-    placeholder: 'Compose an epic...',
-    theme: 'snow'
+	modules: {
+		toolbar: {
+			container: [
+				[{
+					header: [1, 2, false]
+				}],
+				['bold', 'italic', 'underline'],
+				['image', 'code-block'],
+				['mathLive']
+			],
+			handlers: {
+				'mathLive': insert,
+			}
+		}
+	},
+	placeholder: 'Compose an epic...',
+	theme: 'snow'
 });
 
 mathLiveBlot.register(Quill);
 
-var toolbar = quill.getModule('toolbar');
+/*var toolbar = quill.getModule('toolbar');
 toolbar.addHandler('mathLive', function() {
-  console.log('mathLive')
-});
+	console.log('mathLive')
+});*/
 
 /*var mathLiveButton = document.querySelector('.ql-mathLive');
 mathLiveButton.addEventListener('click', insert);*/
 
 function insert() {
-    let selection = quill.getSelection();
-    console.log(selection);
-    quill.insertEmbed(selection.index, "mathLive", "x");
+	let selection = quill.getSelection();
+	console.log(selection);
+	quill.insertEmbed(selection.index, "mathLive", "x");
 }
 
 document.getElementById("insert").addEventListener("click", insert);
